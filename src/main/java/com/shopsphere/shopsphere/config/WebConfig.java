911b8ac6177239d4c1d42c27244f97e6d2ae3e13
Bhavana <Bhavana0071@gmail.com>
2026-09-12 10:00:36 +0530

    Fix infinite redirect loop in WebConfig

--- src/main/java/com/shopsphere/shopsphere/config/WebConfig.java
+++ src/main/java/com/shopsphere/shopsphere/config/WebConfig.java
@@ -9,9 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/{path:^(?!api).*$}")
-                .setViewName("forward:/index.html");
-        registry.addViewController("/{path:^(?!api).*$}/**")
+        registry.addViewController("/{path:^(?!api|index\\.html|assets).*$}")
                 .setViewName("forward:/index.html");
     }
 }
